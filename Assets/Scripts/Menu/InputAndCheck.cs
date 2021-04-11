917f2df88bbdccd8b3b531ce95deb73cfe3f1964
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-11 11:44:54 +0700

    Menambahkan Manager dan Pengecekan Jawaban
    script, maupun game object dari manager sudah ditambah.

---
+++ Assets/Scripts/Menu/InputAndCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputAndCheck : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _inputJawabanText;
+    [SerializeField] private TextMeshProUGUI _displayAnswerText;
+
+    private GameObject _objectToGuess;
+
+    private void Start()
+    {
+        _objectToGuess = GameObject.Find("Kubus");
+    }
+
+    // Cek apakah jawaban benar
+    public void OnInputChangeEnd()
+    {
+        if (_inputJawabanText.text.Contains(_objectToGuess.name))
+        {
+            _displayAnswerText.text = "Benar";
+        }
+        else
+        {
+            _displayAnswerText.text = "Salah";
+        }
+    }
+}
