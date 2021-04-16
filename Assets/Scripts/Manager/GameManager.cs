a959fded8ef1bff6dc30784e99f90a2be7d0d978
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-16 16:48:49 +0700

    Tambahan

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    // Game Manager mengatur :
+    // Perubahan scene (Scene apa yang harus di-load / ditampilkan ke layar)
+    // Load Region yang dipilih di select region menu ke select level menu.
+    // Memanggil Manager lain
+    // Mengatur game state
+
+    [Header("Managers")]
+    [SerializeField] GameObject UIManager;
+    Canvas _uiManagerCanvas;
+
+    [Header("UI Initialization")]
+    [SerializeField] List<string> SceneName;
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        _uiManagerCanvas = Instantiate(UIManager).GetComponent<Canvas>();
+        _uiManagerCanvas.worldCamera = mainCamera;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
-    
+    // UI Manager mengatur
+    // Bagaimana User Interface setiap scene (button apa yang harus ditampilkan, konten seperti apa, dll)
+
+    [Header("User Interface")]
+    [SerializeField] GameObject _appBar;
+    [SerializeField] GameObject _selectMainMenu;
+    [SerializeField] GameObject _selectRegion;
+    [SerializeField] GameObject _selectLevel;
+    [SerializeField] GameObject _gameplay;
+    [SerializeField] GameObject _levelComplete;
+    [SerializeField] GameObject _bottomNav;
+
+    private void Start()
+    {
+        _selectMainMenu.SetActive(true);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnChangeScene(GameObject currentUIContent, GameObject nextUIContent)
+    {
+        currentUIContent.SetActive(false);
+        nextUIContent.SetActive(true);
+    }
 }
