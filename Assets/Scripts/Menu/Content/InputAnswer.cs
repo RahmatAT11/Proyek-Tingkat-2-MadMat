e48e8e0b239b16b4696ce81b94c81f5e53043a9a
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-26 16:27:22 +0700

    Add simple checking mechanism for Gameplay

---
+++ Assets/Scripts/Menu/Content/InputAnswer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InputAnswer : MonoBehaviour
+{
+    [SerializeField] TMP_InputField _input;
+    [SerializeField] TextMeshProUGUI _textEdit;
+
+    [SerializeField] Image _answer;
+    [SerializeField] Sprite _right;
+    [SerializeField] Sprite _wrong;
+
+    [SerializeField] Image _picture;
+
+    public void OnFinishEditing()
+    {
+        string textEditString = _textEdit.text;
+        string pictureName = _picture.sprite.name;
+
+        if (textEditString.Contains(pictureName))
+        {
+            _answer.sprite = _right;
+        }
+        else
+        {
+            _answer.sprite = _wrong;
+        }
+    }
+}
