b4a045ace0b0689f9146d236d310449e938955d0
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-05-25 16:53:07 +0700

    Menambah UI Home
    Main Menu dan
    Option Menu

---
+++ Assets/Scripts/Controller/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeController : MonoBehaviour
+{
+    #region Inisialisasi Variabel
+    [Header("Variabel Button")]
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _optionButton;
+    [SerializeField] private Button _quitButton;
+
+    [Header("Variabel User Interface")]
+    [SerializeField] private GameObject _optionMenu;
+
+    private void Awake()
+    {
+        //_playButton.onClick.AddListener();
+        //_optionButton.onClick.AddListener();
+        //_quitButton.onClick.AddListener();
+    }
+    #endregion
+}
