b6a2ee6507f66a9c6a6e3b092b27b5f1e14b67c5
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-05-25 15:54:53 +0700

    Mengganti font baru dan tampilan

---
+++ Assets/Scripts/Controller/QuizController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuizController : MonoBehaviour
+{
+    #region Inisialisasi Variabel
+    [Header("Variabel User Interface")]
+    [SerializeField] private Image _quizImage;
+    [SerializeField] private TMP_InputField _quizInput;
+    [SerializeField] private Image _quizCorrectImage;
+
+    [Header("Quiz Correct Image Sprite")]
+    [SerializeField] private Sprite _quizCorrectSprite;
+    [SerializeField] private Sprite _quizWrongSprite;
+
+    private Color transparent = new Color(255, 255, 255, 0);
+    private Color normal = Color.white;
+
+    private void Awake()
+    {
+        _quizCorrectImage.sprite = _quizCorrectSprite;
+        _quizCorrectImage.color = transparent;
+    }
+    #endregion
+
+
+}
